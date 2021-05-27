8bc3cc5aa427971027666f884d558955448f4ec1
tca <tcarland@gmail.com>
2021-05-27 15:16:01 -0700

    fix up k8s wrapper

--- bin/k8s-ip-services-run.sh
+++ bin/k8s-ip-services-run.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 #
+MC="${MC:-callisto}"
 
-APP_JAR="s3a://spark/jars/ip-services-0.3.2-jar-with-dependencies.jar"
+APP_JAR="ip-services-0.3.2-jar-with-dependencies.jar"
+APP_JAR_PATH="s3a://spark/jars"
 APP_CLASS="com.trace3.spark.IpServices"
 
-( mc cp target/$APP_CLASS $MC/spark/jars )
+( mc cp target/$APP_JAR $MC/spark/jars/ )
 if [ $? -ne 0 ]; then
     echo "Error in `mc cp`"
     exit 1
 fi
 
-sparkonk8s.sh ipservices $APP_CLASS $APP_JAR $@ )
+sparkonk8s.sh ipservices $APP_CLASS $APP_JAR_PATH/$APP_JAR $@ 
 
