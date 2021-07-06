2e658ac58146e55530bb17a31c16815ceed7db0a
tca <tcarland@gmail.com>
2021-07-05 17:12:31 -0700

    sync

--- bin/k8s-ip-services-run.sh
+++ bin/k8s-ip-services-run.sh
@@ -3,6 +3,7 @@
 # sparkonk8s.sh -m 2g -w 4 ipservices com.trace3.spark.IpServices s3a://spark/jars/ip-services-0.3.4-jar-with-dependencies.jar s3a://scratch/services s3a://spark/ipservices
 #
 MC="${MC:-callisto}"
+HIVE_NS="${HIVE_NAMESPACE:-trino}"
 
 cwd=$(dirname "$(readlink -f "$0")")
 . $cwd/ipservices-config.sh
@@ -10,19 +11,21 @@ cwd=$(dirname "$(readlink -f "$0")")
 jar_path="s3a://spark/jars"
 app_class="com.trace3.spark.IpServicesTable"
 
-( mc cp target/$IPSERVICES_JAR $MC/spark/jars/ )
-
+# copy jar
+jar=$(mc ls $MC/spark/jars/$IPSERVICES_JAR 2>/dev/null)
+if [ -z "$jar" ]; then
+    ( mc cp target/$IPSERVICES_JAR $MC/spark/jars/ )
+fi 
 if [ $? -ne 0 ]; then
     echo "Error in `mc cp`"
     exit 1
 fi
-
 
 if ! sparkonk8s.sh -e >/dev/null; then
     echo "Error with the spark run configuration."
     exit 1
 fi
 
-sparkonk8s.sh -H trino ipservices $app_class $jar_path/$IPSERVICES_JAR $@ 
+sparkonk8s.sh -H $HIVE_NS ipservices $app_class $jar_path/$IPSERVICES_JAR $@ 
 
 exit $?
