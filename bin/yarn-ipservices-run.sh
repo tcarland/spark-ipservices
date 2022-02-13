1ad6953bfe369a9bdcc4794f4fa3b3477eedbcc0
tca <tcarland@gmail.com>
2022-02-13 07:11:25 -0800

    sync

--- bin/yarn-ipservices-run.sh
+++ bin/yarn-ipservices-run.sh
@@ -8,6 +8,7 @@ cwd=$(dirname "$(readlink -f "$0")")
 APP_CLASS="com.trace3.spark.IpServices"
 
 spark-submit --master yarn \
+  --deploy-mode cluster \
   --class $IPSERVICES_CLASS \
   target/$IPSERVICES_JAR \
   $@
