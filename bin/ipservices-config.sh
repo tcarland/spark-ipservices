81ff86a91e41560b8c19187f2415ecfafc607b84
tca <tcarland@gmail.com>
2022-07-31 07:35:00 -0700

    update to spark 3.2.2

--- bin/ipservices-config.sh
+++ bin/ipservices-config.sh
@@ -1,7 +1,7 @@
 # defaults for launching ipservices 
 
-export IPSERVICES_VERSION="0.3.5"
-export SPARK_VERSION="3.1.2"
+export IPSERVICES_VERSION="0.3.6"
+export SPARK_VERSION="3.2.2"
 
 
 IPSERVICES_JAR="ip-services-${IPSERVICES_VERSION}-jar-with-dependencies.jar"
