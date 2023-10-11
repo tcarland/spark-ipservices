ba55d7d34a8f8c61859966fb47aff0e0e6191759
Timothy C. Arland <tcarland@gmail.com>
2023-10-11 06:13:14 -0600

    sync

--- bin/ipservices-config.sh
+++ bin/ipservices-config.sh
@@ -1,7 +1,7 @@
 # defaults for launching ipservices 
 
 export IPSERVICES_VERSION="0.4.1"
-export SPARK_VERSION="3.3.2"
+export SPARK_VERSION="3.3.3"
 
 
 IPSERVICES_JAR="ip-service-names-${IPSERVICES_VERSION}-jar-with-dependencies.jar"
